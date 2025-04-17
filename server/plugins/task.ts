6ac5bafe147770b9b80eb1cf157474e49a6a7301
kolyasick <vana63393@gmail.com>
2025-04-17 14:40:07 +0300

    status fix

--- server/plugins/task.ts
+++ server/plugins/task.ts
@@ -6,6 +6,7 @@ export default defineNitroPlugin((nitroApp) => {
     try {
       const now = new Date();
       const oneMinute = new Date(now.getTime() - 1 * 60 * 1000);
+      const twoMinutes = new Date(now.getTime() - 2 * 60 * 1000);
 
       const users = await prisma.profile.findMany({
         select: {
@@ -16,12 +17,22 @@ export default defineNitroPlugin((nitroApp) => {
       });
 
       for (const user of users) {
-        if (user.lastSeen < oneMinute && user.online) {
+        if (user.lastSeen < twoMinutes && user.online) {
           await prisma.profile.update({
             where: { id: user.id },
             data: { online: false },
+            select: {
+              online: true,
+            },
           });
-          console.log(`Пользователь ${user.id} помечен как оффлайн - сервер`);
+        } else if (user.lastSeen < oneMinute && !user.online) {
+          await prisma.profile.update({
+            where: { id: user.id },
+            data: { online: true },
+            select: {
+              online: true,
+            },
+          });
         }
       }
     } catch (error) {
